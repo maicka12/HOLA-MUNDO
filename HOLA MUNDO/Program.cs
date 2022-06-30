05569ae04abcd087c97867d04c6027a4e7b95dab
Asus <Asus@PC>
2022-07-01 01:26:13 +0200

    Agregar archivos de proyecto.

---
+++ HOLA MUNDO/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleAplicattion5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO¡¡¡");
+            Console.ReadLine(); // para la ejecución hasta presionar teclar
+        }
+    }
+}
